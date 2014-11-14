9f3d73b1724590328b972fe1e78900a16f2d9b0b
Carlos Agüero <caguero@osrfoundation.org>
2014-11-14 10:29:23 -0800

    Checking ruby installation in cmake.

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -5,6 +5,13 @@ include (${project_cmake_dir}/FindOS.cmake)
 include (FindPkgConfig)
 
 ########################################
+# The ruby Interpreter.
+find_package(Ruby REQUIRED)
+if (NOT RUBY_FOUND)
+  BUILD_ERROR ("Missing: ruby (ruby)")
+endif()
+
+########################################
 # Include man pages stuff
 include (${project_cmake_dir}/Ronn2Man.cmake)
 add_manpage_target()
