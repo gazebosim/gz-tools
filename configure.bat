8cfbfbbb7058b620a4c7577f5e92438b63275e01
Carlos Agüero <caguero@osrfoundation.org>
2016-05-11 17:03:45 -0700

    Testing windows support.

---
+++ configure.bat
@@ -0,0 +1,16 @@
+@rem Run CMake, pointing to sibling directories containing dependencies.
+
+@set build_type=Release
+@if not "%1"=="" set build_type=%1
+
+@set build_bitness=64
+@if not "%2"=="" set build_bitness=%2
+
+@echo Configuring for build type %build_type% for %build_bitness% bits
+cmake -G "NMake Makefiles"^
+      -DCMAKE_INSTALL_PREFIX="install/%build_type%"^
+      -DCMAKE_BUILD_TYPE="%build_type%"^
+      ..
+
+@if %errorlevel% neq 0 exit /b %errorlevel%
+@echo Configuration complete.  To build, run `nmake`
