6cef069d05528c7b4bbeafd737f98b26e69282aa
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-06-19 21:38:29 +0200

    Remove ruby find_program and ronn. Not really used
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -3,15 +3,3 @@ include (CheckCXXSourceCompiles)
 
 include (${project_cmake_dir}/FindOS.cmake)
 include (FindPkgConfig)
-
-########################################
-# The ruby Interpreter.
-find_program(RUBY ruby)
-if (NOT RUBY)
-  BUILD_ERROR ("Missing: ruby (ruby)")
-endif()
-
-########################################
-# Include man pages stuff
-include (${project_cmake_dir}/Ronn2Man.cmake)
-add_manpage_target()
