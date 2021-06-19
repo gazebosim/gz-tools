d3b153c3e0bd124fcf107da1b9f66c41872a2dc9
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-06-19 21:52:12 +0200

    Remove Utils.cmake code related to test
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -117,26 +117,6 @@ macro (ign_setup_apple)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-undefined -Wl,dynamic_lookup")
 endmacro()
 
-# This should be migrated to more fine control solution based on set_property APPEND
-# directories. It's present on cmake 2.8.8 while precise version is 2.8.7
-link_directories(${PROJECT_BINARY_DIR}/test)
-include_directories("${PROJECT_SOURCE_DIR}/test/gtest/include")
-
-#################################################
-# Enable tests compilation by default
-if (NOT DEFINED ENABLE_TESTS_COMPILATION)
-  set (ENABLE_TESTS_COMPILATION True)
-endif()
-
-# Define testing macros as empty and redefine them if support is found and
-# ENABLE_TESTS_COMPILATION is set to true
-macro (ign_build_tests)
-endmacro()
-
-if (ENABLE_TESTS_COMPILATION)
-  include (${project_cmake_dir}/TestUtils.cmake)
-endif()
-
 #################################################
 # Macro to setup supported compiler warnings
 # Based on work of Florent Lamiraux, Thomas Moulard, JRL, CNRS/AIST.
